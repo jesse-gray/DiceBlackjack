d629a3a4336739be25ec035997645ff47d712792
Jesse Gray <mail.jessegray@gmail.com>
2019-10-06 13:56:03 +1300

    Aligned GameEngineTest methods with other class' method names appropriately

--- diceblackjack/src/test/java/nz/ac/eit/GameEngineTest.java
+++ diceblackjack/src/test/java/nz/ac/eit/GameEngineTest.java
@@ -21,57 +21,50 @@ public class GameEngineTest {
 
     @Test
     public void checkScore_whileScoreIs21_shouldReturnFalse () {
-        // TODO: 06-Oct-19
-        gameEngine.getUser().setScore(21);
+        gameEngine.getUser().setPlayerScore(21);
         boolean checkResult = gameEngine.checkScoreValid(gameEngine.getUser());
         assertEquals("The player score of 21 should return false", false, checkResult);
     }
 
     @Test
     public void checkScore_whileScoreIs22_shouldReturnFalse () {
-        // TODO: 06-Oct-19
-        gameEngine.getUser().setScore(22);
+        gameEngine.getUser().setPlayerScore(22);
         boolean checkResult = gameEngine.checkScoreValid(gameEngine.getUser());
         assertEquals("The player score of 21 should return false", false, checkResult);
     }
 
     @Test
     public void dealerTurn_shouldReturnBetween16and27 () {
-        // TODO: 06-Oct-19
         gameEngine.dealerTurn();
-        int dealerScore = gameEngine.getDealer().getScore();
+        int dealerScore = gameEngine.getDealer().getPlayerScore();
         assertTrue("The dealer should finish on a score of at least 16 and no more than 27", dealerScore >= 16 && dealerScore <= 27);
     }
 
     @Test
     public void hit_shouldReturnBetween2And12 () {
-        // TODO: 06-Oct-19
-        int preScore = gameEngine.getUser().getScore();
+        int preScore = gameEngine.getUser().getPlayerScore();
         gameEngine.hit(gameEngine.getUser());
-        int postScore = gameEngine.getUser().getScore();
+        int postScore = gameEngine.getUser().getPlayerScore();
         int scoreDifference = postScore - preScore;
-        assertTrue("Hit should add between 2 and 12 to score", scoreDifference <=2 && scoreDifference <= 12);
+        assertTrue("Hit should add between 2 and 12 to score", scoreDifference >=2 && scoreDifference <= 12);
     }
 
     @Test
     public void calculateWinner_whileScoresAre0_shouldReturnNull () {
-        // TODO: 06-Oct-19
         Player winner = gameEngine.calculateWinner();
         assertNull("While scores are the same, winner should be null", winner);
     }
 
     @Test
     public void calculateWinner_whileUserScoreIsHigher_shouldReturnUser () {
-        // TODO: 06-Oct-19
-        gameEngine.getUser().setScore(1);
+        gameEngine.getUser().setPlayerScore(1);
         Player winner = gameEngine.calculateWinner();
         assertEquals("While user score is higher, user should win", gameEngine.getUser(), winner);
     }
 
     @Test
     public void calculateWinner_whileUserScoreIs22_shouldReturnDealer () {
-        // TODO: 06-Oct-19
-        gameEngine.getUser().setScore(22);
+        gameEngine.getUser().setPlayerScore(22);
         Player winner = gameEngine.calculateWinner();
         Player dealer = gameEngine.getDealer();
         assertEquals("While user score is over 21, dealer should win", dealer, winner);
@@ -79,8 +72,7 @@ public class GameEngineTest {
 
     @Test
     public void calculateWinner_whileDealerScoreIsHigher_shouldReturnDealer () {
-        // TODO: 06-Oct-19
-        gameEngine.getDealer().setScore(1);
+        gameEngine.getDealer().setPlayerScore(1);
         Player winner = gameEngine.calculateWinner();
         Player dealer = gameEngine.getDealer();
         assertEquals("While user dealer is higher, dealer should win", dealer, winner);
@@ -88,8 +80,7 @@ public class GameEngineTest {
 
     @Test
     public void calculateWinner_whileDealerScoreIs22_shouldReturnUser () {
-        // TODO: 06-Oct-19
-        gameEngine.getDealer().setScore(22);
+        gameEngine.getDealer().setPlayerScore(22);
         Player winner = gameEngine.calculateWinner();
         Player dealer = gameEngine.getDealer();
         assertEquals("While user score is higher, user should win", dealer, winner);
