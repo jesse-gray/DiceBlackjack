384ea0cfb9d40296033e9f337877c1b0868ede05
sjdolley <garfunkalman2@gmail.com>
2019-10-13 18:02:54 +1300

    Test class for Die

--- diceblackjack/src/test/java/nz/ac/eit/DieTest.java
+++ diceblackjack/src/test/java/nz/ac/eit/DieTest.java
@@ -5,3 +5,23 @@ import org.junit.Test;
 
 import static org.junit.Assert.*;
 
+public class DieTest {
+    Private Die die;
+
+
+    @Before
+    public void setup() {
+        die = new Die();
+    }
+
+
+    // single test to check the math.random() function produces a result between 1 and 6
+    @Test
+    public void checkDie_whileRollIsBetween1and6_shouldReturnTrue() {
+        die.roll();
+        int result = die.getValue();
+        assertTrue("A single Die roll should produce a result of 1-6", result >=1 && result <= 6);
+
+    }
+}
+
